1b2f1421843fe3ac2168385effc2bc2af1518f8e
Navjot Singh <singh.navjot12@gmail.com>
2017-02-12 03:18:38 +0530

    Configuring for images of celebrities

--- cam.py
+++ cam.py
@@ -2,8 +2,8 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-f = np.load('./data/my_face_data.npy')
-f2 = np.load('./data/my_face_labels.npy')
+f = np.load('./data/face_data.npy')
+f2 = np.load('./data/face_labels.npy')
 X_train = f.reshape(f.shape[0],f.shape[1]*f.shape[2])   #dimensions of training set
 y_train = f2    #labels
 
@@ -23,7 +23,7 @@ def get_name(x, k=5):
     return pred_arr[0][pred]
 
 def recognize_face(im):
-    im = cv2.resize(im, (100, 100))                     #http://stackoverflow.com/questions/31996367/opencv-resize-fails-on-large-image-with-error-215-ssize-area-0-in-funct
+    im = cv2.resize(im, (200, 200))                     #http://stackoverflow.com/questions/31996367/opencv-resize-fails-on-large-image-with-error-215-ssize-area-0-in-funct
     im = im.flatten()
 
     return get_name(im)
@@ -37,19 +37,20 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 while True:
     _, fr = rgb.read()
     
-    gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)             #converting camera feed to grayscale because our images are in grayscale
-    # print gray.shape
-    faces = facec.detectMultiScale(gray, 1.3, 5)
-    
-    for (x,y,w,h) in faces:
-        fc = fr[x:x+w, y:y+h, :]
-        out = recognize_face(cv2.cvtColor(fc, cv2.COLOR_BGR2GRAY))
-        cv2.putText(fr, out, (x, y), font, 1, (255, 255, 0), 2)
-    	cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
-    
-    cv2.imshow('rgb', fr)
-    #cv2.imshow('gray', gray)
-    if cv2.waitKey(1) == 27:
-        break
+    if _ == 1:
+        gray = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)             #converting camera feed to grayscale because our images are in grayscale
+        # print gray.shape
+        faces = facec.detectMultiScale(gray, 1.3, 5)
+        
+        for (x,y,w,h) in faces:
+            fc = fr[x:x+w, y:y+h, :]
+            out = recognize_face(cv2.cvtColor(fc, cv2.COLOR_BGR2GRAY))
+            cv2.putText(fr, out, (x, y), font, 1, (255, 255, 0), 2)
+            cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+
+        cv2.imshow('rgb', fr)
+        #cv2.imshow('gray', gray)
+        if cv2.waitKey(1) == 27:
+            break
 
 cv2.destroyAllWindows()
